4b7816119f1e24d5e1934fb50459363912a222d5
AntonAcorn <ShustikovAnimation@gmail.com>
2022-11-17 14:30:34 +0300

    Create operation

--- src/main/java/ru/acorn/CRUDapp/controllers/PeopleController.java
+++ src/main/java/ru/acorn/CRUDapp/controllers/PeopleController.java
@@ -3,30 +3,44 @@ package ru.acorn.CRUDapp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.acorn.CRUDapp.dao.PersonDao;
+import ru.acorn.CRUDapp.models.Person;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
     private final PersonDao personDao;
+
     @Autowired
     public PeopleController(PersonDao personDao) {
         this.personDao = personDao;
     }
 
     @GetMapping
-    public String index(Model model){ //получаем всех людей в dao и передаем в представление
+    public String index(Model model) { //получаем всех людей в dao и передаем в представление
         model.addAttribute("people", personDao.index());
         return "people/index"; //папка и файл из webapp
     }
+
     @GetMapping("/{id}") //@PathVariable извлекает id из Url, т.е. в int id будель лежать число из url запроса
-    public String show(@PathVariable("id") int id, Model model){
+    public String show(@PathVariable("id") int id, Model model) {
         //получаем одного человека из dao и передаем его на отображение во вью
         model.addAttribute("person", personDao.show(id));
         return "people/show";
     }
+
+    @GetMapping("/new") //запрос на форму создания человека
+    public String newPerson(Model model) {  //в данном случае в модель будет передоваться все то, что мы занесли в форму
+        model.addAttribute("person", new Person()); //т.е. создаем пустую заглушку
+        return "people/new";     //можно все заменить на @ModelAttribute, спринг сам создаст пустой объект и положит его в модель
+    }
+
+    @PostMapping()      //в @ModelAttribute будет лежать Person с данными из формы
+    public String create (@ModelAttribute("person")Person person) {
+        personDao.save(person);
+        return "redirect:/people";  //ключевое слово redirect перенаправляет на /people
+        }
 }
+
--- src/main/java/ru/acorn/CRUDapp/dao/PersonDao.java
+++ src/main/java/ru/acorn/CRUDapp/dao/PersonDao.java
@@ -13,16 +13,21 @@ public class PersonDao {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT , "Joe"));
+        people.add(new Person(++PEOPLE_COUNT, "Joe"));
         people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT,"Ton"));
+        people.add(new Person(++PEOPLE_COUNT, "Ton"));
     }
 
-    public List<Person> index(){
+    public List<Person> index() {
         return people;
     }
 
-    public Person show(int id){
-       return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
     }
 }
