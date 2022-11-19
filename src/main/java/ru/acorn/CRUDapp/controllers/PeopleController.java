af574af8dfbca389397858ba1deea79cff6aba50
AntonAcorn <ShustikovAnimation@gmail.com>
2022-11-19 03:29:11 +0300

    Validator + Pattern Validator

--- src/main/java/ru/acorn/CRUDapp/controllers/PeopleController.java
+++ src/main/java/ru/acorn/CRUDapp/controllers/PeopleController.java
@@ -7,17 +7,21 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.acorn.CRUDapp.dao.PersonDAO;
 import ru.acorn.CRUDapp.models.Person;
+import ru.acorn.CRUDapp.util.PersonValidator;
 
 import javax.validation.Valid;
+
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping()
@@ -40,6 +44,7 @@ public class PeopleController {
     @PostMapping()
     public String create(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult) {
+        personValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors())
             return "people/new";
 
@@ -56,6 +61,7 @@ public class PeopleController {
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
                          @PathVariable("id") int id) {
+        personValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors())
             return "people/edit";
 
